8b902f17d32f26f33936b886077e209346bc93ad
Антон Сахаров <av.sakharov.888@gmail.com>
2022-07-16 16:00:16 +0300

    Last version

--- homework.py
+++ homework.py
@@ -85,9 +85,9 @@ class Running(Training):
         coeff_calorie_1 = 18
         coeff_calorie_2 = 20
         spent_calories = (
-            (coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2) *
-            self.weight / self.M_IN_KM * (self.duration * 60)
-         )
+            (coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2)
+            * self.weight / self.M_IN_KM * (self.duration * 60)
+        )
         return spent_calories
 
 
@@ -112,9 +112,9 @@ class SportsWalking(Training):
         coeff_calorie_2 = 2
         coeff_calorie_3 = 0.029
         spent_calories = (
-            (coeff_calorie_1 * self.weight + (self.get_mean_speed() **
-             coeff_calorie_3 // self.height) * coeff_calorie_2 *
-             self.weight) * (self.duration * 60)
+            (coeff_calorie_1 * self.weight + (self.get_mean_speed()
+             ** coeff_calorie_3 // self.height) * coeff_calorie_2
+             * self.weight) * (self.duration * 60)
         )
         return spent_calories
 
@@ -150,8 +150,8 @@ class Swimming(Training):
         coeff_calorie_1 = 1.1
         coeff_calorie_2 = 2
         spent_calories = (
-            (self.get_mean_speed() + coeff_calorie_1) *
-            coeff_calorie_2 * self.weight
+            (self.get_mean_speed() + coeff_calorie_1)
+            * coeff_calorie_2 * self.weight
         )
         return spent_calories
 
